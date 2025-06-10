69378c3757cd7bf023bf14abdcce2454a2b1df8f
Daniel Holmes <daniel.holmes97@hotmail.com>
2025-06-10 14:22:20 +0100

    Found some logs finally

--- api/get-currency-list.ts
+++ api/get-currency-list.ts
@@ -4,10 +4,12 @@ const fetchCurrencyList = async () => {
   const response = await fetch(
     "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json"
   ).then((res) => res.json())
+  console.log(response)
   if (!response) {
     const secondResponse = await fetch(
       "https://latest.currency-api.pages.dev/v1/currencies.json"
     ).then((res) => res.json())
+    console.log(secondResponse)
     return secondResponse
   }
   return response
