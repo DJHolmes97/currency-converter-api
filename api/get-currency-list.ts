0f36f7083a1f6dc79e55b97d2d90be41c7cf202d
Daniel Holmes <daniel.holmes97@hotmail.com>
2025-06-10 14:02:37 +0100

    setup new serverless function

---
+++ api/get-currency-list.ts
@@ -0,0 +1,19 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+
+const fetchCurrencyList = async () => {
+  const response = await fetch(
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json"
+  ).then((res) => res.json())
+  if (!response) {
+    const secondResponse = await fetch(
+      "https://latest.currency-api.pages.dev/v1/currencies.json"
+    ).then((res) => res.json())
+    return secondResponse
+  }
+  return response
+}
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const response = fetchCurrencyList()
+  return res.json(response)
+}
